e763d84b97d6670a611b235ca15de5ad7f96e7a2
Szwajcii <d.szwajkos@gmail.com>
2019-02-09 16:19:25 +0100

    Initialize spring project.

---
+++ src/main/java/com/codecool/krk/datalizer/DatalizerApplication.java
@@ -0,0 +1,14 @@
+package com.codecool.krk.datalizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatalizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatalizerApplication.class, args);
+    }
+
+}
+
