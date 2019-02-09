c7c1bfbb288dbf4fb47c3295cd9fe23ea20cdb9e
Szwajcii <d.szwajkos@gmail.com>
2019-02-09 21:34:09 +0100

    Add method to handle redirect to home page of the app. Delete JPA dependency.

--- src/main/java/com/codecool/krk/datalizer/controller/WebController.java
+++ src/main/java/com/codecool/krk/datalizer/controller/WebController.java
@@ -1,10 +1,14 @@
 package com.codecool.krk.datalizer.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
 
-
+    @GetMapping("/home")
+    public String handleIndex() {
+        return "index";
+    }
 
 }
