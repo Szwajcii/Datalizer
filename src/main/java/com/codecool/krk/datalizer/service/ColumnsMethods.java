8a786e1856c3e6eadab7dedc34e75ce05b80d74b
Szwajcii <d.szwajkos@gmail.com>
2019-02-13 18:56:44 +0100

    Add additional categories to generate random data.

--- src/main/java/com/codecool/krk/datalizer/service/ColumnsMethods.java
+++ src/main/java/com/codecool/krk/datalizer/service/ColumnsMethods.java
@@ -26,15 +26,28 @@ public class ColumnsMethods {
         functions.put("last_name", this::getRandomSecondName);
         functions.put("nick_name", this::getRandomNickName);
 
+        functions.put("full_name", this::getRandomFullName);
+
+        functions.put("male_name", this::getRandomMaleName);
+        functions.put("female_name", this::getRandomFemaleName);
+
+        functions.put("age", this::getRandomAge);
+
+        functions.put("job_title", this::getRandomJobTitle);
+        functions.put("language", this::getRandomLanguage);
+
         //Contact data
-        functions.put("phone", this::getRandomPhoneNumber);
+        functions.put("phone_number", this::getRandomPhoneNumber);
         functions.put("email", this::getRandomEmail);
 
 
         //Address
-        functions.put("street", this::getRandomStreet);
+        functions.put("street_name", this::getRandomStreetName);
+        functions.put("street_number", this::getRandomStreetNumber);
         functions.put("city", this::getRandomCity);
         functions.put("country", this::getRandomCountry);
+
+
 
 
     }
