34e77f41612f9eb16bccd5b5432c42a48bd1274e
Szwajcii <d.szwajkos@gmail.com>
2019-02-13 18:36:24 +0100

    Initialize class with methods corresponding to column names.

---
+++ src/main/java/com/codecool/krk/datalizer/service/ColumnsMethods.java
@@ -0,0 +1,34 @@
+package com.codecool.krk.datalizer.service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
+public class ColumnsMethods {
+
+    private Map<String, Supplier<String>> functions;
+    private static int idUser = 1;
+
+    public ColumnsMethods() {
+        initializeMethodsMap();
+    }
+
+    public Map<String, Supplier<String>> getFunctions(){
+        return this.functions;
+    }
+
+
+    private void initializeMethodsMap() {
+        functions = new HashMap<>();
+
+        //Personal data
+        functions.put("first_name", this::getRandomFirstName);
+        functions.put("last_name", this::getRandomSecondName);
+        functions.put("nick_name", this::getRandomNickName);
+
+
+
+
+    }
+
+}
