b94f5a5ffa9ba6b7e1eae5a77e57d46f01e9c901
Szwajcii <d.szwajkos@gmail.com>
2019-03-04 12:40:48 +0100

    Implement getData method in DataGenerator class.

--- src/main/java/com/codecool/krk/datalizer/service/DataGenerator.java
+++ src/main/java/com/codecool/krk/datalizer/service/DataGenerator.java
@@ -1,4 +1,22 @@
 package com.codecool.krk.datalizer.service;
 
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+
 public class DataGenerator implements IDataGenerator {
+
+
+    private static Map<String, Supplier<String>> functionMap = new ColumnsMethods().getFunctions();
+
+    public static String getData(List<String> columns, int quantity) {
+
+        StringBuilder sb = new StringBuilder();
+
+
+
+        return sb.toString();
+    }
+
+
 }
