55b646c61becf52d4f9bc64dedd78dc01f5b67af
Szwajcii <d.szwajkos@gmail.com>
2019-03-05 09:46:24 +0100

    Implement getters, setters and toString method in ColumnList class.

--- src/main/java/com/codecool/krk/datalizer/model/ColumnList.java
+++ src/main/java/com/codecool/krk/datalizer/model/ColumnList.java
@@ -1,4 +1,27 @@
 package com.codecool.krk.datalizer.model;
 
+import java.util.ArrayList;
+
 public class ColumnList {
+
+    private ArrayList<Column> columnList;
+
+    public ColumnList(ArrayList<Column> columnList) {
+        this.columnList = columnList;
+    }
+
+    public ArrayList<Column> getColumnList() {
+        return this.columnList;
+    }
+
+    public void setColumnList(ArrayList<Column> columnList) {
+        this.columnList = columnList;
+    }
+
+    @Override
+    public String toString() {
+        return "ColumnList{" +
+                "columnList=" + columnList +
+                '}';
+    }
 }
